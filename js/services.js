bed433b281fba1af6598d6c633c7ebf10810f4f4
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2019-11-28 18:38:52 +0400

    partfinish

--- js/services.js
+++ js/services.js
@@ -1,29 +1,79 @@
-$(function(){
+$(function () {
 
-  $('.services-item').mouseenter(function() {
-    const elemNum = $(this).index() + 1
+    $('.services-item').mouseenter(function () {
+        const elemNum = $(this).index() + 1
 
-    if(elemNum > 9) {
+        var width = $(window).width();
+        console.log("TCL: width", width)
+        if (width <= 768) {
+            if (elemNum > 9) {
+                $(this).find('.services-item__block').css({
+                    marginLeft: '33px'
+                })
+            } else {
+                $(this).find('.services-item__block').css({
+                    marginLeft: '12px'
+                })
+            }
+        } else {
+            if (elemNum > 9) {
+                $(this).find('.services-item__block').css({
+                    marginLeft: '85px'
+                })
+            } else {
+                $(this).find('.services-item__block').css({
+                    marginLeft: '40px'
+                })
+            }
+        }
+
+
+    })
+
+    $('.services-item').mouseleave(function () {
+
         $(this).find('.services-item__block').css({
-            marginLeft: '85px'
+            marginLeft: '0'
         })
-    } else {
-        $(this).find('.services-item__block').css({
-            marginLeft: '40px'
+    })
+
+
+    $(".seo-btn").click(function () {
+        $(this).hide()
+        $('.seo-text-inner').css({
+            width: '100%',
+            height: 'auto'
         })
-    }
+    })
 
-  })
+    $(window).scroll(function () {
 
-  $('.services-item').mouseleave(function() {
-    
-      $(this).find('.services-item__block').css({
-          marginLeft: '0'
-      })
-  })
+        var header = $('#header__main').outerHeight(),
+            side = $("#side").outerHeight(),
+            contentHeight = $("#content").outerHeight(),
+            contentTopOfBottom = $("#content").offset().top - $(window).scrollTop() - header + contentHeight,
+            contentTop = $("#content").offset().top - $(window).scrollTop() - header,
+            sidebar = $("#sidebar").offset().top - $(window).scrollTop() - (header + 10)
 
-  
+        if (sidebar < 0 && contentTopOfBottom >= side) {
 
-  
+            $("#side").css('top', -sidebar)
+
+        }
+
+        if (sidebar > 0) {
+
+            $("#side").css('top', 0)
+
+        }
+
+        // if (contentTopOfBottom >= side) {
+
+        //     $("#side").css('top', contentTop)
+
+        // }
+
+    })
+
 
 })
