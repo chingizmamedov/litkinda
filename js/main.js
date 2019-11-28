c62cabba9e6db47f1f38cd6517b838bf4968c848
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2019-11-28 04:31:11 +0400

    set night

--- js/main.js
+++ js/main.js
@@ -32,51 +32,9 @@ $(document).ready(function () {
 
     
 
-    $('.services-item').mouseenter(function() {
-        const elemNum = $(this).index() + 1
+    
 
-        if(elemNum > 9) {
-            $(this).find('.services-item__block').css({
-                marginLeft: '85px'
-            })
-        } else {
-            $(this).find('.services-item__block').css({
-                marginLeft: '40px'
-            })
-        }
-
-    })
-
-    $('.services-item').mouseleave(function() {
-       
-        $(this).find('.services-item__block').css({
-            marginLeft: '0'
-        })
-    })
-
-    $(window).scroll(function(){
-
-        var headerHeight = $('#header__main').outerHeight()
-        console.log("TCL: headerHeight", headerHeight + 5)
-        
-
-        var offsetTop = $('.aside').offset().top - $(window).scrollTop() - (headerHeight + 5)
-        // console.log("TCL: offsetTop", offsetTop)
-        
-        if(offsetTop < 0 ) {
-            
-            // $('.aside-content').css('marginTop', -offsetTop)
-            // $('.aside-content').css('position', 'fixed')
-
-        } else {
-
-            $('.aside-content').css({
-                marginTop: 0
-            })
-
-        }
-
-    });
+    
 
 });
 
--- js/services.js
+++ js/services.js
@@ -1,50 +1,29 @@
 $(function(){
 
-  var offsetOne = 0;
-  var offsetTwo = 0;
-  
-  if ($('.main').length > 0) offsetOne = $('.main').offset().top;
-  if ($('.main__first').length > 0) offsetTwo = $('.main__first').offset().top;
-  offsetDiff = offsetTwo - offsetOne;
+  $('.services-item').mouseenter(function() {
+    const elemNum = $(this).index() + 1
 
-  $('.callback').css('marginTop', offsetDiff + 'px')
-
-  var showedwhencalldp = false;
-  var showedadp = false;
-  var showedadplast = false;
-  var scrolled = false;
-  $(window).on('scroll',function() {
-
-    if ($('.main').length > 0) var offsetOne = $('.main').offset().top;
-    if ($('.main__first').length > 0) offsetTwo = $('.main__first').offset().top;
-    
-      var offsetDiff = offsetTwo - offsetOne,
-      offsetMainBlock = offsetTwo - $(window).scrollTop(),
-      heightMain = $('.main').height(),
-      heightCallbackBlock = $('.callback').height(),
-      needMain = heightMain + offsetOne - $(window).scrollTop()
-
-    if(offsetMainBlock < 0 && needMain > heightCallbackBlock) {
-      $('.callback').css('marginTop', offsetDiff + -offsetMainBlock + 'px')
+    if(elemNum > 9) {
+        $(this).find('.services-item__block').css({
+            marginLeft: '85px'
+        })
+    } else {
+        $(this).find('.services-item__block').css({
+            marginLeft: '40px'
+        })
     }
 
-    if(offsetTwo - $(window).scrollTop() > 0) {
-      $('.callback').css('marginTop', offsetDiff + 'px')
-    }
+  })
+
+  $('.services-item').mouseleave(function() {
     
-     if (showedwhencalldp == true) {
-          $('.when_call_date').datepicker().data('datepicker').hide();  
-          showedwhencalldp = true;
-     }
-     if (showedadp == true) {
-          $('.arenda-datepicker').datepicker().data('datepicker').hide();  
-          showedadp = true;
-     }
-     if (showedadplast == true) {
-          $('.arenda-datepicker-last').datepicker().data('datepicker').hide();  
-          showedadplast = true;
-     }
-     scrolled = true; 
-  });
+      $(this).find('.services-item__block').css({
+          marginLeft: '0'
+      })
+  })
+
   
+
+  
+
 })
